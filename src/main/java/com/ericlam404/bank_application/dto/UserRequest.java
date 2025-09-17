27998efda1b914368c547006b07ffbf36e6a86da
EricLam404 <96885224+EricLam404@users.noreply.github.com>
2025-09-17 17:32:50 -0400

    Add inital Dto and entity

---
+++ src/main/java/com/ericlam404/bank_application/dto/UserRequest.java
@@ -0,0 +1,25 @@
+package com.ericlam404.bank_application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+}
---
+++ src/main/java/com/ericlam404/bank_application/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ericlam404.bank_application.repository;
+
+import com.ericlam404.bank_application.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
